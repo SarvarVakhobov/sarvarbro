afc7c1da72130fa70b70b655d61081cc86c23a1e
YuldShah <notyuldshah@gmail.com>
2025-01-18 22:16:05 +0500

    switching to windows

--- utils/yau.py
+++ utils/yau.py
@@ -1,5 +1,7 @@
 from loader import db
 from .chan_info import checksub
+import random
+import string
 
 async def notsubbed(userid) -> list:
     channels = db.fetchall("SELECT title, chid, link FROM channel")
@@ -8,3 +10,7 @@ async def notsubbed(userid) -> list:
         if not await checksub(userid, ch[1]):
             new_chs.append(ch)
     return new_chs
+
+def g_code():
+    characters = string.ascii_letters + string.digits  # Letters and digits
+    return ''.join(random.sample(characters, 6))
